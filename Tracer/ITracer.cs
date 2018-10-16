7f7eb8f23d4fbea017bc9a99c5def9d6787296e2
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-10-17 01:26:24 +0300

    Created ITracer interface and added implementation for him.

---
+++ Tracer/ITracer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracer
+{
+    public interface ITracer
+    {
+        // Called to start measurement.
+        void StartTrace();
+
+        // Called to stop measurement.
+        void StopTrace();
+
+        // Called to get the measurement result.
+        TraceResult GetTraceResult();
+    }
+}
