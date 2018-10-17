51aad6821388ffd006e12d74004fa50ec383d3b7
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-10-18 01:12:17 +0300

    Added implementation for TracedMethod class.

--- Tracer/TracedMethod.cs
+++ Tracer/TracedMethod.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +8,49 @@ namespace Tracer
 {
     public class TracedMethod
     {
+        private List<TracedMethod> nestedMethods;
+        private Stopwatch stopwatch;
 
+        internal TracedMethod()
+        {
+            nestedMethods = new List<TracedMethod>();
+            stopwatch = new Stopwatch();
+        }
+
+        public string MethodName
+        {
+            get;
+            private set;
+        }
+
+        public string MethodClassName
+        {
+            get;
+            private set;
+        }
+
+        public ulong LeadTime
+        {
+            get => (ulong)stopwatch.ElapsedMilliseconds;
+            private set { }
+        }
+
+        public string LeadTimeToString
+        {
+            get => String.Format("{0} ms.", LeadTime);
+            private set { }
+        }
+
+        public List<TracedMethod> NestedMethods
+        {
+            get => new List<TracedMethod>(nestedMethods);
+            private set { }
+        }
+
+        internal void StartTrace() => stopwatch.Start();
+
+        internal void StopTrace() => stopwatch.Stop();
+
+        internal void AddNestedMethodToList(TracedMethod tracedMethod) => nestedMethods.Add(tracedMethod);
     }
 }
