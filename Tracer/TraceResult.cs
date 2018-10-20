d337cba1a887d04c7fa41cc5fe140d33e20284f9
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-10-20 23:29:13 +0300

    Added implementation for TraceResult class.

--- Tracer/TraceResult.cs
+++ Tracer/TraceResult.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,27 @@ namespace Tracer
 {
     public class TraceResult
     {
+        private ConcurrentDictionary<int, TracedThread> traceResults;
 
+        public List<TracedThread> TraceResults
+        {
+            get => new List<TracedThread>(new SortedDictionary<int, TracedThread>(traceResults).Values);
+            private set { }
+        }
+
+        internal TracedThread AddOrGetTraceResult(int id)
+        {
+            TracedThread tracedThread;
+            if (!traceResults.TryGetValue(id, out tracedThread))
+            {
+                tracedThread = new TracedThread(id);
+                traceResults[id] = tracedThread;
+            }
+            return tracedThread;
+        }
+
+        internal TracedThread GetThreadResult(int id) => traceResults[id];
+
+        internal TraceResult() => traceResults = new ConcurrentDictionary<int, TracedThread>();
     }
 }
