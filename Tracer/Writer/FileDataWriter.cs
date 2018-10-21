ca32a7c5d1e9dddaa2e3b9e88a08acc35e2fa285
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-10-21 18:22:48 +0300

    Added implementation for FileDataWriter class.

--- Tracer/Writer/FileDataWriter.cs
+++ Tracer/Writer/FileDataWriter.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tracer.Serialization;
 
 namespace Tracer.Writer
 {
-    class FileDataWriter
+    public class FileDataWriter : IWriter
     {
+        private readonly string _filename;
+
+        public void Write(TraceResult traceResult, ISerializer serializer)
+        {
+            using(FileStream fileStream = new FileStream(_filename, FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                serializer.Serialize(traceResult, fileStream);
+            }
+        }
+
+        internal FileDataWriter(string filename)
+        {
+            _filename = filename;
+        }
     }
 }
