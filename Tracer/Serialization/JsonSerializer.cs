b34f3cd1a2635cdea86943693b193d1029d273d2
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-10-21 17:13:13 +0300

    Added implementation for JsonSerializer class.

--- Tracer/Serialization/JsonSerializer.cs
+++ Tracer/Serialization/JsonSerializer.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Xml;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+using System.IO;
 
 namespace Tracer.Serialization
 {
     public class JsonSerializer : ISerializer
     {
+        private readonly DataContractJsonSerializer jsonSerializer;
+
+        public void Serialize(TraceResult traceResult, Stream stream)
+        {
+            using(XmlDictionaryWriter jsonWriter = JsonReaderWriterFactory.CreateJsonWriter(stream, Encoding.UTF8, true, true))
+            {
+                jsonSerializer.WriteObject(jsonWriter, traceResult);
+            }
+        }
+
+        internal JsonSerializer()
+        {
+            jsonSerializer = new DataContractJsonSerializer(typeof(TraceResult));
+        }
     }
 }
