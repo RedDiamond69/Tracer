7153b56c1d0fedecca68e35f5b7e915f81bc6c54
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-10-21 16:21:34 +0300

    Created ISerializer interface and added implementation for him.

---
+++ Tracer/Serialization/ISerializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tracer.Serialization
+{
+    public interface ISerializer
+    {
+        void Serialize(TraceResult traceResult, Stream stream);
+    }
+}
