2534f4e3aae753e2f7d03093c5b3bc8efc3dba4f
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-10-22 01:19:21 +0300

    Complete Tracer and an example of use.

--- Tracer/TraceResult.cs
+++ Tracer/TraceResult.cs
@@ -2,8 +2,6 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Tracer
 {
--- Tracer/TracedMethod.cs
+++ Tracer/TracedMethod.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Runtime.Serialization;
-using System.Threading.Tasks;
 
 namespace Tracer
 {
