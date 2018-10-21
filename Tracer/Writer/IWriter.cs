6317704b427eea7e40896b3c485299e64caf0ea6
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-10-21 16:27:39 +0300

    Created IWriter interface and added implementation for him.

---
+++ Tracer/Writer/IWriter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tracer.Serialization;
+
+namespace Tracer.Writer
+{
+    public interface IWriter
+    {
+        void Write(TraceResult traceResult, ISerializer serializer);
+    }
+}
