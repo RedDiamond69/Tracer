363a1cbcb4213c0cceb0773cf805131fd00aba17
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-10-21 18:26:08 +0300

    Added implementation for ConsoleDataWriter class.

--- Tracer/Writer/ConsoleDataWriter.cs
+++ Tracer/Writer/ConsoleDataWriter.cs
@@ -1,12 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tracer.Serialization;
 
 namespace Tracer.Writer
 {
-    public class ConsoleDataWriter
+    public class ConsoleDataWriter : IWriter
     {
+        public void Write(TraceResult traceResult, ISerializer serializer)
+        {
+            using(Stream outputStream = Console.OpenStandardOutput())
+            {
+                serializer.Serialize(traceResult, outputStream);
+            }
+        }
     }
 }
