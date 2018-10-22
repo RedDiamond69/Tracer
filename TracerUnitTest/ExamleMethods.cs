d841c50524dcf263dce92265153c1beea7b678b7
Aliaksei Kasiuk <reddiamond821@gmail.com>
2018-10-23 00:42:12 +0300

    Added implementation for ExampleMethods class.

--- TracerUnitTest/ExamleMethods.cs
+++ TracerUnitTest/ExamleMethods.cs
@@ -2,11 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using Tracer;
 
 namespace TracerUnitTest
 {
     public class ExamleMethods
     {
+        private readonly ITracer _tracer;
+
+        public ExamleMethods(ITracer tracer)
+        {
+            _tracer = tracer;
+        }
+
+        public void SimpleMethod()
+        {
+            _tracer.StartTrace();
+            Thread.Sleep(10);
+            _tracer.StopTrace();
+        }
+
+        public void WithNestedMethods()
+        {
+            _tracer.StartTrace();
+            SimpleMethod();
+            Thread.Sleep(10);
+            _tracer.StopTrace();
+        }
     }
 }
